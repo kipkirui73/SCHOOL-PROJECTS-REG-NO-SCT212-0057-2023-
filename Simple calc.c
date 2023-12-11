25a1afc59e4cf48801194e9e65adef76a768cbfe
kipkirui73 <146317065+kipkirui73@users.noreply.github.com>
2023-12-11 16:42:13 +0300

    Add files via upload

---
+++ Simple calc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(void)
+{
+    int a,b ,c;
+    printf("enter your  first value");
+    scanf("%d",&a);
+    printf("enter your second value");
+    scanf("%d",&b);
+    c = a+b ;
+    printf("%d+%d=%d \n",a,b,c);
+    return 0;
+}
